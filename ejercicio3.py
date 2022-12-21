228264b9a27cd8c3c872c7ceed399a2a02502950
Lenin <cmoposisan1@educacion.navarra.es>
2022-12-21 19:19:48 +0100

    actualizacion

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,7 +2,6 @@
 # lea el fichero tabla-n.txt con la tabla de multiplicar de
 # ese número, y muestre por pantalla la línea m del fichero.
 # Si el fichero no existe debe mostrar un mensaje por pantalla informando de ello.
-
 import os
 
 
